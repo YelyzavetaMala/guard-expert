5b0f5cd6e6b1455b58fe326ad98c3fcb816eca59
YelyzavetaMala <malaelizaveta678@gmail.com>
2025-06-04 16:20:22 +0300

    added desk media and tablet

--- src/main.js
+++ src/main.js
@@ -95,7 +95,7 @@ initResponsiveSwiper(".swiper-four", {
 initResponsiveSwiper(".swiper-five", {
   375: { slidesPerView: 1, spaceBetween: 16 },
   834: { slidesPerView: 2, spaceBetween: 20 },
-  1440: { slidesPerView: 3, spaceBetween: 24 },
+  1440: { slidesPerView: 3, spaceBetween: 100 },
 });
 initResponsiveSwiper(".swiper-six", {
   375: { slidesPerView: 1, spaceBetween: 16 },
